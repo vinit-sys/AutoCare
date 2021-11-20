4d8c06926deb7456290cf55a16abe46def7226d7
vinit-sys <vinit.kumar.cs18@gmail.com>
2021-11-20 13:24:30 +0530

    login and reister attached

---
+++ AutoCare/accounts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+# Create your views here.
+def index(request):
+    return render(request,"login.html")
+def login(request):
+    if request.method=="POST":
+        username=request.POST.get("username")
+        password=request.POST.get("pass")
+        user=authenticate(username=username,password=password)
+        if user is not None:
+            return HttpResponse("welocme")
+        else:
+            return HttpResponse("try again")
+    else:
+        return redirect("/")
+def register(request):
+    if request.method=="POST":
+        username=request.POST.get("username")
+        email=request.POST.get("email")
+        password=request.POST.get("pass")
+        User.objects.create_user(username=username,email=email,password=password)
+        return HttpResponse("registered")
+    else:
+        return redirect("/")
