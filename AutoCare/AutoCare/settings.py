a8bb47baae83711caf695b76acdabf6830f6714d
vinit-sys <vinit.kumar.cs18@gmail.com>
2022-02-18 11:51:30 +0530

    added pages

--- AutoCare/AutoCare/settings.py
+++ AutoCare/AutoCare/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,8 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,"static")
 ]
 
+MEDIA_ROOT=os.path.join(BASE_DIR,"media")
+MEDIA_URL='/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
